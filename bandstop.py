1e305535ef6a524b461d1bf5b9828534f364dac1
Juan Dominguez <juan.dominguez.developer@gmail.com>
2020-05-03 00:48:28 -0700

    Implement second-order sections filter

--- bandstop.py
+++ bandstop.py
@@ -50,14 +50,26 @@ def plot_freq_response(num_poly, den_poly, fs):
     ax[1].grid()
     plt.show()
 
+def calculate_edge_freqs(f_remove, bandwidth, fs):
+    """
+        returns the lower and higher edge of a filter passband, normalized to half cycles / sample
+        (freq / (fs / 2)) 
+    """
+    low = f_remove - bandwidth/2.0
+    high = f_remove + bandwidth/2.0
+    
+    nyq_freq = fs / 2.0
+    low = low / nyq_freq  
+    high = high / nyq_freq
+
+    return (low, high)
+    
 def bandstop_iirfilter(f_remove, bandwidth, order, fs, filter_type='butter'):
     """
         Design an IIR (infinite impulse response) bandstop filter using scipy.signal.iirfilter
         
         :param f_remove: the center frequency of the band
         :param bandwidth: the bandwidth of the filter 
-            (low = f_remove - (bandwidth / 2)), normalized according to the nyquist frequency (fs / 2)
-            (high = f_remove + (bandwidth / 2)), normalized according to the nyquist frequency (fs / 2)
         :param order: the order of the filter
         :param fs: the sampling frequency of the signal 
         :param filter_type: the type of filter, default is 'butter'
@@ -65,44 +77,58 @@ def bandstop_iirfilter(f_remove, bandwidth, order, fs, filter_type='butter'):
         :returns b: the numerator polynomial of the filter
         :returns a: the denominator polynomial of the filter
     """
-    nyq  = fs/2.0
-    low  = f_remove - bandwidth/2.0
-    high = f_remove + bandwidth/2.0
 
-    # normalize edge frequencies according to (half cycles / sample) 
-    # (freq / (fs / 2)) where fs / 2 = nyq
-    low  = low/nyq
-    high = high/nyq
+    low, high = calculate_edge_freqs(f_remove, bandwidth, fs)
 
     b, a = signal.iirfilter(order, [low, high], btype='bandstop',
                      analog=False, ftype=filter_type)
     return (b, a)
 
-def bandstop_butter(f_remove, bandwidth, order, fs):
+def channels_to_stereo(left_channel, right_channel):
+    """
+        takes two row vector channels and converts them to a single, stereo signal
+    """
+    return np.column_stack((left_channel.transpose(), right_channel.transpose()))
+
+def bandstop_butter(sig, f_remove, bandwidth, order, fs, stereo=True):
     """
         Design an IIR (infinite impulse response) butterworth bandstop filter using scipy.signal.butter
+        and filter the given signal
         
+        :param sig: the signal to filter
         :param f_remove: the center frequency of the band
         :param bandwidth: the bandwidth of the filter 
-            (low = f_remove - (bandwidth / 2)), normalized according to the nyquist frequency (fs / 2)
-            (high = f_remove + (bandwidth / 2)), normalized according to the nyquist frequency (fs / 2)
         :param order: the order of the filter
         :param fs: the sampling frequency of the signal 
 
-        :returns b: the numerator polynomial of the filter
-        :returns a: the denominator polynomial of the filter
+        :returns the filtered signal as a numpy array
     """
-    nyq  = fs/2.0
-    low  = f_remove - bandwidth/2.0
-    high = f_remove + bandwidth/2.0
+    low, high = calculate_edge_freqs(f_remove, bandwidth, fs)
 
-    # normalize edge frequencies according to (half cycles / sample) 
-    # (freq / (fs / 2)) where fs / 2 = nyq
-    low  = low/nyq
-    high = high/nyq
     b, a = signal.butter(order, [low, high], btype='bandstop')
-    return (b, a)
+    if not stereo:
+        return signal.filtfilt(b, a, sig)
+    
+    left_channel = signal.filtfilt(b, a, sig[:, 0])
+    right_channel = signal.filtfilt(b, a, sig[:, 1])
 
+    return channels_to_stereo(left_channel, right_channel)
+
+def bandstop_butter_sos(sig, f_remove, bandwidth, order, fs, stereo=True):
+    """
+        same as bandstop_butter but uses second-order sections (sosfiltfilt), which have 'fewer 
+        numerical problems'
+    """
+    low, high = calculate_edge_freqs(f_remove, bandwidth, fs)
+    sos = signal.butter(order, [low, high], btype='bandstop', output='sos')
+
+    if not stereo:
+        return signal.sosfiltfilt(sos, sig)
+    
+    left_channel = signal.sosfiltfilt(sos, sig[:, 0])
+    right_channel = signal.sosfiltfilt(sos, sig[:, 1])
+
+    return channels_to_stereo(left_channel, right_channel)
 
 if __name__ == '__main__':
     file = 'GY01/trimmed.wav'
@@ -111,13 +137,8 @@ if __name__ == '__main__':
     f_remove = 5000
     bandwidth = 250
 
-    b, a = bandstop_butter(f_remove, bandwidth, order, fs)
-    plot_freq_response(b, a, fs)
-
-    # notch_filtered = signal.lfilter(b, a, sig)
-    notch_filtered_left = signal.filtfilt(b, a, sig[:, 0])
-    notch_filtered_right = signal.filtfilt(b, a, sig[:, 1])
-    filt_sig = np.column_stack((notch_filtered_left.transpose(), notch_filtered_right.transpose()))
+    filt_sig = bandstop_butter_sos(sig, f_remove, bandwidth, order, fs)
+    print(filt_sig.shape)
     # if you want to write it back as a WAV file
     # filt_sig = np.asarray(filt_sig, dtype=np.int16)
     # wavfile.write('GY01/filtered_notch.WAV', fs, filt_sig)
@@ -131,9 +152,9 @@ if __name__ == '__main__':
     ax_sig[1].specgram(sig[:, 1], NFFT=NFFT, Fs=fs, noverlap=900)
     ax_sig[1].set_title('Original Signal Right Channel')
 
-    ax_filtered[0].specgram(notch_filtered_left, NFFT=NFFT, Fs=fs, noverlap=900)
+    ax_filtered[0].specgram(filt_sig[:, 0], NFFT=NFFT, Fs=fs, noverlap=900)
     ax_filtered[0].set_title('Filtered Signal Left Channel')
-    ax_filtered[1].specgram(notch_filtered_right,NFFT=NFFT, Fs=fs, noverlap=900)
+    ax_filtered[1].specgram(filt_sig[:, 1], NFFT=NFFT, Fs=fs, noverlap=900)
     ax_filtered[1].set_title('Filtered Signal Right Channel')
     plt.show()
 
