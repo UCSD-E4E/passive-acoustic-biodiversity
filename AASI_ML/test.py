d0fa07226b8b4451c09567e7232295eb80249264
Tudictator <jacobthescreenwriter@gmail.com>
2020-07-19 14:05:26 -0700

    Batch classification implemented

--- AASI_ML/test.py
+++ AASI_ML/test.py
@@ -4,35 +4,56 @@ import os
 import sys
 import numpy as np
 import datetime
+import matplotlib.dates as mdates
+import pdb
 
 audio_dir = sys.argv[1]
 detector = RNNDetector()
+local_scores = []
+global_scores = []
 
 for wav_file in os.listdir(audio_dir):
     try:
-        global_score, local_score = detector.predict_on_wav(wav_file)
+        global_score, local_score = detector.predict_on_wav(audio_dir + wav_file)
+        print("Loaded", wav_file)
     except:
         print("Error on file", wav_file)
+        global_score = [0]
+        local_score = [0]*22501
+        continue
 
-    print(wav_file)
-    print("Global score:", global_score)
-    print("Local score:", local_score)
+    local_scores.append(local_score)
+    global_scores.extend(global_score)
+    
+with open("local_scores_AM15.txt", "w") as f:
+    for arr in local_scores:
+        for el in arr:
+            f.write(str(el) + ",")
+        f.write("\n")
 
-dates = ["06/17/19", "06/18/19", "06/19/19", "06/20/19", "06/21/19"]
-x_vals = [datetime.datetime.strptime(d,"%m/%d/%Y").date() for d in dates]
-ax = plt.gca()
+with open("global_scores_AM15.txt", "w") as f:
+    for sc in global_scores:
+        f.write(str(sc) + '\n')
 
-formatter = mdates.DateFormatter("%Y-%m-%d")
-ax.xaxis.set_major_formatter(formatter)
+# dates = ["06/17/2019", "06/18/2019", "06/19/2019", "06/20/2019", "06/21/2019"]
+# x_vals = [datetime.datetime.strptime(d,"%m/%d/%Y").date() for d in dates]
+# ax = plt.gca()
 
-locator = mdates.DayLocator()
-ax.xaxis.set_major_locator(locator)
+# formatter = mdates.DateFormatter("%Y-%m-%d")
+# ax.xaxis.set_major_formatter(formatter)
 
-plt.figure(figsize=(15, 8))
-plt.plot(x_vals, local_score, lw=1)
-plt.title("Bird Vocalizations from 00:00 6/17 - 23:50 6/21")
-plt.xlabel("Time")
-plt.ylabel("Prediction scores")
-plt.ylim(0,1.0)
+# locator = mdates.DayLocator()
+# ax.xaxis.set_major_locator(locator)
 
-plt.savefig("local_score_AM1.png")
+# y_vals = global_scores
+# y_vals = local_scores
+# x_vals = np.arange(0, len(y_vals))
+
+# plt.figure(figsize=(15, 8))
+# plt.plot(x_vals, y_vals)
+# plt.title("Bird Vocalizations from 00:00 6/17 - 23:50 6/21")
+# plt.xlabel("Time")
+# plt.ylabel("Prediction scores")
+# plt.ylim(0,1.0)
+
+# plt.savefig("local_score_AM15_2.png")
