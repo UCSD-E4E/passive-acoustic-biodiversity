91fe87cd1e62cb6416e2ba1234d1e13c0f19ed5d
Gabriel Steinberg <gsteinb1@binghamton.edu>
2020-07-19 21:09:46 -0400

    Bird viz vocalizations working

--- AASI_ML/test.py
+++ AASI_ML/test.py
@@ -3,8 +3,6 @@ import matplotlib.pyplot as plt
 import os
 import sys
 import numpy as np
-import datetime
-import matplotlib.dates as mdates
 import pdb
 
 audio_dir = sys.argv[1]
@@ -34,26 +32,3 @@ with open("local_scores_AM15.txt", "w") as f:
 with open("global_scores_AM15.txt", "w") as f:
     for sc in global_scores:
         f.write(str(sc) + '\n')
-
-# dates = ["06/17/2019", "06/18/2019", "06/19/2019", "06/20/2019", "06/21/2019"]
-# x_vals = [datetime.datetime.strptime(d,"%m/%d/%Y").date() for d in dates]
-# ax = plt.gca()
-
-# formatter = mdates.DateFormatter("%Y-%m-%d")
-# ax.xaxis.set_major_formatter(formatter)
-
-# locator = mdates.DayLocator()
-# ax.xaxis.set_major_locator(locator)
-
-# y_vals = global_scores
-# y_vals = local_scores
-# x_vals = np.arange(0, len(y_vals))
-
-# plt.figure(figsize=(15, 8))
-# plt.plot(x_vals, y_vals)
-# plt.title("Bird Vocalizations from 00:00 6/17 - 23:50 6/21")
-# plt.xlabel("Time")
-# plt.ylabel("Prediction scores")
-# plt.ylim(0,1.0)
-
-# plt.savefig("local_score_AM15_2.png")
