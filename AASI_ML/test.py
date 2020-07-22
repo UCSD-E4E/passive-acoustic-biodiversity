5c7f57940eb2fad2e6bfe5a93303972bc2862688
Tudictator <jacobthescreenwriter@gmail.com>
2020-07-22 11:33:59 -0700

    Updated file paths in microfaune test.py

--- AASI_ML/test.py
+++ AASI_ML/test.py
@@ -7,7 +7,8 @@ import datetime
 import matplotlib.dates as mdates
 import pdb
 
-audio_dir = sys.argv[1]
+AM_NAME = "AM16"
+audio_dir = AM_NAME + "_Clips/"
 detector = RNNDetector()
 local_scores = []
 global_scores = []
@@ -25,35 +26,12 @@ for wav_file in os.listdir(audio_dir):
     local_scores.append(local_score)
     global_scores.extend(global_score)
     
-with open("local_scores_AM15.txt", "w") as f:
+with open("local_scores_"+AM_NAME+".txt", "w") as f:
     for arr in local_scores:
         for el in arr:
             f.write(str(el) + ",")
         f.write("\n")
 
-with open("global_scores_AM15.txt", "w") as f:
+with open("global_scores_"+AM_NAME+".txt", "w") as f:
     for sc in global_scores:
         f.write(str(sc) + '\n')
-
-# dates = ["06/17/2019", "06/18/2019", "06/19/2019", "06/20/2019", "06/21/2019"]
-# x_vals = [datetime.datetime.strptime(d,"%m/%d/%Y").date() for d in dates]
-# ax = plt.gca()
-
-# formatter = mdates.DateFormatter("%Y-%m-%d")
-# ax.xaxis.set_major_formatter(formatter)
-
-# locator = mdates.DayLocator()
-# ax.xaxis.set_major_locator(locator)
-
-# y_vals = global_scores
-# y_vals = local_scores
-# x_vals = np.arange(0, len(y_vals))
-
-# plt.figure(figsize=(15, 8))
-# plt.plot(x_vals, y_vals)
-# plt.title("Bird Vocalizations from 00:00 6/17 - 23:50 6/21")
-# plt.xlabel("Time")
-# plt.ylabel("Prediction scores")
-# plt.ylim(0,1.0)
-
-# plt.savefig("local_score_AM15_2.png")
