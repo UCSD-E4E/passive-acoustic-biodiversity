63591b62c5a83c959c3b8e8a76e3db884d5dd8f8
pranjalc1 <86537904+pranjalc1@users.noreply.github.com>
2022-07-19 19:30:47 -0700

    Changed which files were used, and cleaned up all of the dataframes Moved birdnet_species.csv to main folder to check which species are already in birdnet Moved annotation post processing from PyHa to allow to chunk annotations Changed the sample choosing code to reflect species allowed by birdnet Changed xc_annotations and embeddings (due to file changes) Updated code in Embeddings_Work.ipynb notebook to include data cleaning and changing manual ids from common name to scientific name to avoid differences between manual and model annotations

--- BirdnetEmbeddings/choosing_mixed_bird_sample.py
+++ BirdnetEmbeddings/choosing_mixed_bird_sample.py
@@ -1,11 +1,24 @@
 import pandas as pd
 import shutil
+import os
 
 labels = pd.read_csv("mixed_bird_manual.csv")
-file_names = pd.Series(labels["IN FILE"].unique())
+file_names = pd.DataFrame(labels["IN FILE"].unique())
+file_names.columns = ["IN FILE"]
+file_names = file_names.assign(SPECIES = file_names["IN FILE"].apply(lambda x : " ".join(x.split("-")[:2])))
+print(file_names)
+print(f"Length of file_names: {len(file_names)}")
 
-sample_files = file_names.sample(100, random_state = 1)
-print(sample_files)
+birdnet_species = pd.read_csv("birdnet_species.csv")
+birdnet_species.columns = ["SPECIES"]
+birdnet_species = birdnet_species["SPECIES"].apply(lambda x: x.split("_")[0]).tolist()
+print(f"Length of birdnet_species: {len(birdnet_species)}")
+
+file_names = file_names[file_names["SPECIES"].isin(birdnet_species)]
+print(f"New length of file_names: {len(file_names)}")
+
+sample_files = file_names["IN FILE"].sample(100, random_state = 1).tolist()
+print(f"Length of sample_files: {sample_files}")
 
 for file in sample_files:
     shutil.copy("../../Mixed_Bird/" + file, "./data/" + file)
