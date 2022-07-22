99cb93c143d664c978848f148a174f6776701da5
Sean Perry <sean.hyatt.perry@gmail.com>
2022-07-22 10:26:12 -0700

    added majority vote test

--- Pyrenote_clustering/confidence_functions/confidence.py
+++ Pyrenote_clustering/confidence_functions/confidence.py
@@ -1,5 +1,6 @@
 from .statistics import *
 from .clustering import *
+from .chunking import annotation_chunker
 
 def confidence_is_100(df, users):
     return 100
@@ -42,3 +43,13 @@ def get_silhoutte_users_confidence(df,users):
     for file in df["IN FILE"].unique():
         model, clusters,  data_processed, silhoutte = label_clusters(df, DBSCAN_auto_dis_builder_min_dis2, file, distance = 1/2, verbose=False)
         return silhoutte[1]
+
+def majority_vote(df, users, chunk_length=1):
+    iou_scores = np.array([])
+    df = df[df["LAST MOD BY"].isin(users)]
+    df = annotation_chunker(df, chunk_length)
+    df = df.groupby(by=["LAST MOD BY", "OFFSET"]).max().reset_index()
+    #TODO ADD SPECIES STUFF HERE
+    df = df.groupby(by=["OFFSET"]).count().reset_index()
+    counts = (df["FOLDER"]/len(users)).mean() #Does mean make sense here?
+    return counts
