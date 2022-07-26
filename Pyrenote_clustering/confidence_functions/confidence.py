84902e4184beb49b7f0284af1652e086bb999558
Sean Perry <sean.hyatt.perry@gmail.com>
2022-07-26 09:24:29 -0700

    Update confidence.py

--- Pyrenote_clustering/confidence_functions/confidence.py
+++ Pyrenote_clustering/confidence_functions/confidence.py
@@ -19,19 +19,6 @@ def get_pairwise_iou(df, users):
     return iou_scores
 
 
-def get_pairwise_iou(df, users):
-    iou_scores = np.array([])
-    df = df[df["LAST MOD BY"].isin(users)]
-
-    for user in users:
-        user_df = df[df["LAST MOD BY"] == user]
-        not_users_df = df[df["LAST MOD BY"] != user]
-        for not_user in not_users_df["LAST MOD BY"].unique():
-            iou = clip_statistics(user_df,not_users_df,stats_type = "general", threshold = 0.5)["Global IoU"][0]
-            iou_scores = np.append(iou_scores, iou)
-    iou_scores = iou_scores.mean()
-    return iou_scores
-
 def get_silhoutte_confidence(df,users):
     df = df[df["LAST MOD BY"].isin(users)]
     for file in df["IN FILE"].unique():
