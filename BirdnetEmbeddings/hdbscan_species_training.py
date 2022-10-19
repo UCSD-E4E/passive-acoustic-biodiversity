6875599b2b2c7d37021e2ea84de97012b8e90655
pranjalc1 <86537904+pranjalc1@users.noreply.github.com>
2022-10-18 22:59:17 -0700

    Running species-specific training to generate results, updating species-specific notebook to filter based on species label mode instead of noise only

--- BirdnetEmbeddings/hdbscan_species_training.py
+++ BirdnetEmbeddings/hdbscan_species_training.py
@@ -44,11 +44,20 @@ print("Done with data loading!")
 
 def hdbscan_model(embeddings:pd.DataFrame, embeddingColumns:list):
     np.random.seed(42)
+    i = 0
     for species in unique_species:
-        model = HDBSCAN(min_cluster_size = 10, min_samples = 5, cluster_selection_epsilon = 0.5, cluster_selection_method = "leaf")
-        spec_embeddings:pd.DataFrame = embeddings[embeddings["FILE SPECIES"] == species]
+        if (min(5, len(embeddings[embeddings["FILE SPECIES"] == species].index))) <= 1:
+            continue
+        model = HDBSCAN(min_cluster_size = min(5, len(embeddings[embeddings["FILE SPECIES"] == species].index)),
+            min_samples = min(5, len(embeddings[embeddings["FILE SPECIES"] == species].index)),
+            cluster_selection_epsilon = 0.5,
+            cluster_selection_method = "leaf",
+        )
+        spec_embeddings:pd.DataFrame = embeddings[embeddings["FILE SPECIES"] == species].copy()
         model.fit(spec_embeddings[embeddingColumns])
         spec_embeddings["LABELS"] = model.labels_
         spec_embeddings.to_csv(f"./ClusteringModels/species_specific/{species}.csv")
+        i += 1
+        print(f"Done with {i} of {len(unique_species)}")
 
 hdbscan_model(embeddings_df, embeddingColumns)
